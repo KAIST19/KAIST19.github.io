8f8d3b5725a502073de27925ab0f768dbc3d092f
KH Kim <kaist19@kaist.ac.kr>
2023-07-19 14:33:03 +0900

    React updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './img/logo.png';
 import headingImg from './img/meet_our_designer.png';
 import searchIcon from './img/search_icon.png';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <div className="flex-container">
-      <Navbar logo={logo} />
-      <Header headingImg={headingImg} searchIcon={searchIcon} />
+const App = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    // Mocked search results
+    const results = ['Result 1', 'Result 2', 'Result 3'];
+    setSearchResults(results);
+  };
+
+  return (
+    <div className="App">
+      <div className="flex-container">
+        <Navbar logo={logo} />
+        <Header headingImg={headingImg} />
+        <SearchBar 
+          searchIcon={searchIcon} 
+          searchInput={searchInput} 
+          onSearchInputChange={setSearchInput}
+          onSearchSubmit={handleSearch} 
+        />
+        <div className="search-results">
+          {searchResults.map((result, index) => <div key={index}>{result}</div>)}
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 const Navbar = ({ logo }) => (
   <nav className="navbar">
@@ -23,17 +44,23 @@ const Navbar = ({ logo }) => (
   </nav>
 );
 
-const Header = ({ headingImg, searchIcon }) => (
+const Header = ({ headingImg }) => (
   <header className="header">
     <img src={headingImg} alt="Meet our designer" className="heading-image" />
-    <SearchBar searchIcon={searchIcon} />
   </header>
 );
 
-const SearchBar = ({ searchIcon }) => (
+const SearchBar = ({ searchIcon, searchInput, onSearchInputChange, onSearchSubmit }) => (
   <div className="search-bar">
-    <form className="d-flex">
-      <input className="search-input" type="search" placeholder="What are you looking for?" aria-label="Search" />
+    <form className="d-flex" onSubmit={onSearchSubmit}>
+      <input 
+        className="search-input" 
+        type="search" 
+        placeholder="What are you looking for?" 
+        aria-label="Search" 
+        value={searchInput}
+        onChange={e => onSearchInputChange(e.target.value)}
+      />
       <button className="search-button" type="submit">
         <img src={searchIcon} alt="Search icon" />
       </button>
